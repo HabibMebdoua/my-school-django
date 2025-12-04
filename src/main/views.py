183463b5ecc2023d37f84d03f3f1c2007b6d4145
HabibMebdoua <techameur@gmail.com>
2025-12-04 23:52:41 +0100

    add about page

--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 
-
 def home(request):
     return render(request, 'main/index.html')
+
+
+def about(request):
+    """صفحة حول المدرسة"""
+    context = {
+        'school_name': 'متوسطة 5 جويلية 1962',
+        'principal_name': 'Bouzeriba Rebyha Asma',
+        'principal_title': 'استاذة اعلام الي',
+        'location': {
+            'wilaya': 'الطارف',
+            'daira': 'القالة',
+            'village': 'وادي الحوت'
+        },
+        'students_count': 214,
+    }
+    return render(request, 'main/about.html', context)
